d267766a491675deb66644948b8fcd31b57a83ce
Valerii Fedorenko <valerii.fedorenko@thomsonreuters.com>
2020-11-17 23:08:11 +0200

    DefaultQueryParameters

--- Uklon/Client/FinanceYahooClient.cs
+++ Uklon/Client/FinanceYahooClient.cs
@@ -11,13 +11,6 @@ using Uklon.Services;
 
 namespace Uklon.Client
 {
-    public static class DefaultQueryParameters
-    {
-        public const int From = 1;
-        public const long To = 9999999999;
-        public const string Interval = "1d";
-    }
-
     public class FinanceYahooClient
     {
         private readonly HttpClient _client;
---
+++ Uklon/Helpers/DefaultQueryParameters.cs
@@ -0,0 +1,9 @@
+﻿namespace Uklon.Helpers
+{
+    public static class DefaultQueryParameters
+    {
+        public const int From = 1;
+        public const long To = 9999999999;
+        public const string Interval = "1d";
+    }
+}
